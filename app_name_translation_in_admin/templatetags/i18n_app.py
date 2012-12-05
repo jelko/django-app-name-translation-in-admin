4c06d82137bbd5ea3dd3ec634375e76b06fdacd2
J. Arnds <j.arnds@dienetzgestalter.de>
2012-12-05 16:14:20 +0100

    Improved Version. Compatible with Django 1.4

--- app_name_translation_in_admin/templatetags/i18n_app.py
+++ app_name_translation_in_admin/templatetags/i18n_app.py
@@ -1,15 +1,13 @@
 from django import template
-from django.utils.translation import ugettext_lazy as _ 
+from django.utils.translation import ugettext_lazy as _
 
 register = template.Library()
 
 
-@register.filter(name='trans_app')  
+@register.filter(name='trans_app')
 def trans_app(value):
     """
     Utility templatetag filter to avoid double translations of application names.
     """
     return _(value.title())
-
-    
 
